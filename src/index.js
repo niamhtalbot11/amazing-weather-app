0699bb264b15a1c62f56dbedd91f55fe4cd48474
niamhtalbot11 <74904383+niamhtalbot11@users.noreply.github.com>
2024-03-30 13:56:08 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+
+import './index.css';
+import App from './App';
+
+
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+    
+    kjj
+  </StrictMode>
+);
